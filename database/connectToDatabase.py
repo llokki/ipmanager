efa91d00d9d802c74486405c2625fcbd18854715
llokki <llokki92@gmail.com>
2023-04-09 10:28:12 +0300

    add try

--- database/connectToDatabase.py
+++ database/connectToDatabase.py
@@ -2,13 +2,17 @@ import os
 from tortoise import Tortoise
 from tortoise.models import Model
 
-async def db_connect():
-    mysql_root_password = os.getenv('MYSQL_ROOT_PASSWORD','qTPD5ywU')
-    mysql_database = os.getenv('MYSQL_DATABASE','ipmanager')
-    mysql_user = os.getenv('MYSQL_USER','admin1')
-    mysql_password = os.getenv('MYSQL_PASSWORD','5E1f!bEtTQN')
-    mysql_host = os.getenv('MYSQL_HOST','mysql_db')
-    await Tortoise.init(
-        db_url=f'mysql://{mysql_user}:{mysql_password}@{mysql_host}:3307/{mysql_database}',
-        modules={'models': ['app.models_def']}
-    )
+while True:
+    try:
+        async def db_connect():
+            mysql_root_password = os.getenv('MYSQL_ROOT_PASSWORD','qTPD5ywU')
+            mysql_database = os.getenv('MYSQL_DATABASE','ipmanager')
+            mysql_user = os.getenv('MYSQL_USER','admin1')
+            mysql_password = os.getenv('MYSQL_PASSWORD','5E1f!bEtTQN')
+            mysql_host = os.getenv('MYSQL_HOST','mysql_db')
+            await Tortoise.init(
+                db_url=f'mysql://{mysql_user}:{mysql_password}@{mysql_host}:3307/{mysql_database}',
+                modules={'models': ['app.models_def']}
+            )
+    except ConnectionRefusedError:
+        time.sleep(5)
